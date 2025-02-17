0a2923013d748462ea73e2f8ba1d066ba3e9f672
CJ Yetman <cj@cjyetman.com>
2025-02-17 14:17:56 +0100

    Update pacta.loanbook-package.R

--- R/pacta.loanbook-package.R
+++ R/pacta.loanbook-package.R
@@ -6,9 +6,9 @@
 #   All declared Imports should be used.
 ignore_unused_imports <- function() {
   r2dii.data::data_dictionary
-  r2dii.match::data_dictionary
-  r2dii.analysis::data_dictionary
-  r2dii.plot::data_dictionary
+  r2dii.match::match_name
+  r2dii.analysis::target_market_share
+  r2dii.plot::plot_techmix
 }
 
 release_bullets <- function() {
