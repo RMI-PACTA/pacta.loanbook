ecd0b946a7de66d8d0c52ba3859ff2539361b464
CJ Yetman <cj@cjyetman.com>
2025-02-17 16:29:15 +0100

    Update defaults.R

--- .aspell/defaults.R
+++ .aspell/defaults.R
@@ -1,6 +1,6 @@
 # saveRDS(c("PACTA", "pacta"), file = ".aspell/en-pacta.rds", version = 2)
 
-Rd_files <- vignettes <- R_files <- description <-
+Rd_files <- vignettes <- R_files <- dcf <- description <-
   list(encoding = "UTF-8",
        language = "en",
        dictionaries = c("en_stats", "en-pacta"))
