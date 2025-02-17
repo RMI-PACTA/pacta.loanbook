7e7444b3bcbcc67837282aab6881e702b6b9414d
CJ Yetman <cj@cjyetman.com>
2025-02-17 16:53:31 +0100

    Update defaults.R

--- .aspell/defaults.R
+++ .aspell/defaults.R
@@ -1,3 +1,6 @@
+# based on https://ropensci.org/blog/2022/01/21/ropensci-news-digest-january-2022/#to-quote-or-not-to-quote-non-existing-words-in-description
+#
+# add words by running e.g.:
 # saveRDS(c("PACTA", "pacta"), file = ".aspell/pacta.rds", version = 2)
 
 Rd_files <- vignettes <- R_files <- description <-
